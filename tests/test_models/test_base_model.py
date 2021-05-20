a5172887ab3b598a2bb42e1665dd0319b8afc611
darbumas <2314@holbertonschool.com>
2021-05-20 15:07:04 -0500

    add BaseModel unittest

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -1,14 +1,42 @@
 #!/usr/bin/python3
+"""unittest for BaseModel"""
 from models.base_model import BaseModel
+import unittest
+import inspect
+import pep8
 
-my_model = BaseModel()
-my_model.name = "Holberton"
-my_model.my_number = 89
-print(my_model)
-my_model.save()
-print(my_model)
-my_model_json = my_model.to_dict()
-print(my_model_json)
-print("JSON of my_model:")
-for key in my_model_json.keys():
-    print("\t{}: ({}) - {}".format(key, type(my_model_json[key]), my_model_json[key]))
+
+class TestBaseModel(unittest.TestCase):
+    """defining the unittest cases for BaseModel class"""
+
+    @classmethod
+    def setUpClass(cls):
+        """collect members of the class for the doc tests"""
+        cls.setup = inspect.getmembers(BaseModel, inspect.isfunction)
+
+    def test_pep8_conformance(self):
+        """method to test conformity to PEP8."""
+        p8_syle = pep8.StyleGuide(quiet=True)
+        checks = p8_syle.check_files(["./models/base_model.py"])
+        self.assertEqual(checks.total_errors, 0, "Found code style errors "
+                        + "(and warnings).")
+
+    def test_pep8_conformance_test_BaseModel(self):
+        """method to test this file to pep8 conformity"""
+        p8_syle = pep8.StyleGuide(quiet=True)
+        checks = p8_syle.check_files(['tests/test_models/test_base_model.py'])
+        self.assertEqual(checks.total_errors, 1, "Found code style errors "
+                        + "(and warnings).")
+
+    def test_module_docstring(self):
+        """method to test docstring doc for module"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+    def test_class_docstring(self):
+        """method to test docstring doc for classes"""
+        self.assertTrue(len(BaseModel.__doc__) >= 1)
+
+    def test_func_docstrings(self):
+        """method to test docstring doc for functions"""
+        for each in self.setup:
+            self.assertTrue(len(each[1].__doc__) >= 1)
