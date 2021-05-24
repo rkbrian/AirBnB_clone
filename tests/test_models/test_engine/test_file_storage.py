83134bd99f38adb403e3bbc475140c2fe63f1d0b
darbumas <2314@holbertonschool.com>
2021-05-24 11:11:45 -0500

    add test engine

--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,14 +1,17 @@
 #!/usr/bin/python3
 """unittest for FileStorage"""
 from models.engine.file_storage import FileStorage
+from models.base_model import BaseModel
 import unittest
+import os
 import inspect
 import pep8
-
+Storage = FileStorage()
 
 class TestFileStorage(unittest.TestCase):
     """defining the unittest cases for FileStorage class"""
 
+    path = Storage._FileStorage__file_path
     @classmethod
     def setUpClass(cls):
         """collect members of the class for the doc tests"""
@@ -18,25 +21,44 @@ class TestFileStorage(unittest.TestCase):
         """method to test conformity to PEP8."""
         p8_syle = pep8.StyleGuide(quiet=True)
         checks = p8_syle.check_files(["./models/base_model.py"])
-        self.assertEqual(checks.total_errors, 0, "Found code style errors "
-                        + "(and warnings).")
+        self.assertEqual(checks.total_errors, 0, "Found code style errors.")
 
-    def test_pep8_conformance_test_FileStorage(self):
-        """method to test this file to pep8 conformity"""
-        p8_syle = pep8.StyleGuide(quiet=True)
-        checks = p8_syle.check_files(['tests/test_models/test_base_model.py'])
-        self.assertEqual(checks.total_errors, 1, "Found code style errors "
-                        + "(and warnings).")
-
-    def test_module_docstring(self):
+    def test_StorageModuleDoc(self):
         """method to test docstring doc for module"""
         self.assertTrue(len(FileStorage.__doc__) >= 1)
 
-    def test_class_docstring(self):
+    def test_StorageclassDoc(self):
         """method to test docstring doc for classes"""
         self.assertTrue(len(FileStorage.__doc__) >= 1)
 
-    def test_func_docstrings(self):
+    def test_StorageFuncDoc(self):
         """method to test docstring doc for functions"""
         for each in self.setup:
             self.assertTrue(len(each[1].__doc__) >= 1)
+
+    def test_StorageAll(self):
+        """method to test the path and dict"""
+        if os.path.exists(self.path):
+            os.remove(self.path)
+        self.assertIsInstance(Storage.all(), dict)
+
+    def test_StorageNew(self):
+        """method to test if objects are being set with respective keys"""
+        models = Storage.all().copy()
+        models2 = BaseModel()
+        self.assertEqual(models, Storage.all())
+
+    def test_StorageSave(self):
+        """method to test serialization"""
+        if os.path.exists(self.path):
+            os.remove(self.path)
+        models = BaseModel()
+        models.save()
+        self.assertNotEqual(os.path.getsize(self.path), 0)
+
+    def test_StorageDict(self):
+        """method to test dictionary"""
+        self.assertIsInstance(FileStorage._FileStorage__objects, dict)
+
+if __name__ == '__main__':
+    unittest.main()
